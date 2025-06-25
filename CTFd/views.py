d50e4074f5fedbc201e42b5d2012e07eaf12aacf
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-06-25 14:17:15 +0530

    solved setup routing mistake

--- CTFd/views.py
+++ CTFd/views.py
@@ -296,7 +296,7 @@ def setup():
             with app.app_context():
                 cache.clear()
 
-            return redirect(url_for("views.static_html"))
+            return redirect(url_for("views.index"))
         try:
             return render_template("setup.html", state=serialize(generate_nonce()))
         except TemplateNotFound:
